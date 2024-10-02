73238b04aed4ed245414f41a60cb0d21e2996860
Apoorv Shaswat <shaswatapoorv@gmail.com>
2024-10-02 18:23:56 +0530

    build files

--- express/app.js
+++ express/app.js
@@ -12,6 +12,8 @@ const blogRoutes = require("./routes/index");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, "build")));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
