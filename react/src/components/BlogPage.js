cb1c88de96b65fefa9c4fff97ecfa245b4188313
Apoorv Shaswat <shaswatapoorv@gmail.com>
2024-10-13 13:21:10 +0530

    responsive

--- react/src/components/BlogPage.js
+++ react/src/components/BlogPage.js
@@ -7,35 +7,37 @@ export default function BlogPage() {
   const { item } = location.state;
 
   return (
-    <div>
-      <div className="blog1_main">
-        <div className="blog1_main_content">
-          <b>{item.title}</b>
-          <p>{item.overview}</p>
-        </div>
-      </div>
-
-      <div className="blog1_main_panel">
-        <div className="blog1_main_panel_content">
-          <p>Content</p>
+    <div className="blog1_container">
+      
+        <div className="blog1_main">
+          <div className="blog1_main_content">
+            <b>{item.title}</b>
+            <p>{item.overview}</p>
+          </div>
         </div>
 
-        <div className="blog1_main_panel_buttons">
-          <Link to="/Admin">
-            <button type="button" className="btn btn-success">
-              Edit
-            </button>
+        <div className="blog1_main_panel">
+          <div className="blog1_main_panel_content">
+            <p>Content</p>
+          </div>
 
-            <button type="button" className="btn btn-danger">
-              Delete
-            </button>
-          </Link>
+          <div className="blog1_main_panel_buttons">
+            <Link to="/Admin">
+              <button type="button" className="btn btn-success">
+                Edit
+              </button>
+
+              <button type="button" className="btn btn-danger">
+                Delete
+              </button>
+            </Link>
+          </div>
         </div>
-      </div>
 
-      <div className="blog1_main_overview">
-        <p>{item.content}</p>
-      </div>
+        <div className="blog1_main_overview">
+          <p>{item.content}</p>
+        </div>
+      
     </div>
   );
 }
--- react/src/components/Navbar.js
+++ react/src/components/Navbar.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="header">
-      <div className="title"></div>
       <ul className="nav-links">
         <li>
           <Link to="/">Home</Link>
